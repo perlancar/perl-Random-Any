f417ba84abea7778242fbc7ba9fafbcd6d478baf
perlancar (on notebook-dell-xps13-9315-jun2024) <perlancar@gmail.com>
2025-01-07 12:42:52 +0700

    [doc] Discourage the use of rand() and this module, mention Random::Simple

--- lib/Random/Any.pm
+++ lib/Random/Any.pm
@@ -58,7 +58,21 @@ sub import {
 
 This module provides a single export C<rand()> that tries to use
 L<Data::Entropy::Algorithms>'s C<rand()> first and, if that module is not
-available, falls back on the builtin rand().
+available, warns to STDERR and falls back on the builtin C<rand()>.
+
+Note that whenever you can you are encouraged to use C<rand_int()> or
+C<random_int()>, or C<rand_flt()> or C<random_float()> function instead. From
+C<Data::Entropy::Algorithm>'s documentation:
+
+"This function should not be used in any new code, because the kind of output
+supplied by C<rand> is hardly ever the right thing to use. The C<int(rand($n))>
+idiom to generate a random integer has non-uniform probabilities of generating
+each possible value, except when C<$n> is a power of two. For floating point
+numbers, C<rand> can't generate most representable numbers in its output range,
+and the output is biased towards zero. In new code use C<rand_int> to generate
+integers and "rand_flt" to generate floating point numbers."
+
+Also, take a look at L<Random::Simple> instead of this module.
 
 
 =head1 EXPORTS
@@ -84,3 +98,5 @@ Bool. Can be set to provide default value for C<-warn>.
 =head1 SEE ALSO
 
 L<Data::Entropy::Algorithms>
+
+L<Random::Simple>
